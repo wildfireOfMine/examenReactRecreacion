b2b9d898abba5e2576fc56a31dd501983e6ea8fd
wildfireOfMine <iamtheanguera@gmail.com>
2025-04-21 19:29:47 +0200

    Casi hecho Enfrentamientos.jsx

--- src/views/Enfrentamientos.jsx
+++ src/views/Enfrentamientos.jsx
@@ -14,7 +14,22 @@ const Enfrentamientos = () => {
     <>
         <h1>Enfrentamientos</h1>
         {equiposLocales.map((equipo, index) => {
-            if (j % 2 == 0) {
+            if (index == equiposLocales.length -1  ) {
+              const breakLine = (
+                <li key={index}>{equipo.nombre}
+                <br key={index}/>
+                </li>
+              )
+              j++;
+              return breakLine;
+            } else {
+              const equipo2 = <li key={index}>{equipo.nombre}
+              <p>vs</p>
+              </li>
+              j++;
+              return equipo2;
+            }
+            /* if (j % 2 == 0) {
               const equipo2 = <li key={index}>{equipo.nombre}
               <p>vs</p>
               </li>
@@ -32,6 +47,23 @@ const Enfrentamientos = () => {
               j++;
               return breakLine;
             }
+          } else {
+            if (j == localStorage.length -1 ) {
+              const breakLine = (
+                <li key={index}>{equipo.nombre}
+                <br key={index}/>
+                </li>
+              )
+              j++;
+              return breakLine;
+            } else {
+              const equipo2 = <li key={index}>{equipo.nombre}
+              <p>vs</p>
+              </li>
+              j++;
+              return equipo2;
+            } */
+
             
         })}
     </>
